0220bf49a1916290efe5c54eae4567a467948391
mar-bednar <mbedna8@seznam.cz>
2023-02-26 12:02:29 +0100

    refactor

--- src/FitnessEvaluator.go
+++ src/FitnessEvaluator.go
@@ -1,6 +1,6 @@
 package main
 
-const MOVE_EVALUATION_FITNESS_COEF = 0.25
+const MOVE_EVALUATION_FITNESS_COEF = 0.5
 
 type FitnessEvaluator struct {
 	enviroment *Enviroment
@@ -14,15 +14,12 @@ func NewFitnessEvaluator(enviroment *Enviroment) *FitnessEvaluator {
 
 func (evaluator *FitnessEvaluator) FindClosestSafeTile(position Position) Position {
 	var closest = NewPosition(0, 0)
-	var distance = position.DistanceTo(closest)
 	for x := 0; x < evaluator.enviroment.size; x++ {
 		for y := 0; y < evaluator.enviroment.size; y++ {
 			var selected = NewPosition(x, y)
 			if evaluator.enviroment.GetTile(selected).IsSafeZone() {
-				var distanceToSelected = position.DistanceTo(selected)
-				if distanceToSelected < distance {
+				if position.DistanceTo(selected) < position.DistanceTo(closest) {
 					closest = selected
-					distance = distanceToSelected
 				}
 			}
 		}
@@ -32,7 +29,7 @@ func (evaluator *FitnessEvaluator) FindClosestSafeTile(position Position) Positi
 
 func (evaluator *FitnessEvaluator) EvaluateMove(origin Position, next Position) float64 {
 	var closestTileOrigin = evaluator.FindClosestSafeTile(origin)
-	var closestTileNext = evaluator.FindClosestSafeTile(origin)
+	var closestTileNext = evaluator.FindClosestSafeTile(next)
 	var value = closestTileOrigin.DistanceTo(origin) - closestTileNext.DistanceTo(next)
 	return value * MOVE_EVALUATION_FITNESS_COEF
 }
