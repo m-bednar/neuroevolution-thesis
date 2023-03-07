daa5f0e3b2849fb579166223a59a8c0d77e7dad4
mar-bednar <mbedna8@seznam.cz>
2023-03-07 16:38:56 +0100

    refactor

--- src/main.go
+++ src/main.go
@@ -4,10 +4,6 @@ import (
 	"fmt"
 )
 
-const POP_SIZE = 400
-const ENV_SIZE = 15
-const STEPS = ENV_SIZE * 2
-const MUTATION_STRENGTH = 0.2
 const PRINT_EVERY_NTH_GEN = 10
 
 func main() {
@@ -18,10 +14,10 @@ func main() {
 	var enviroment = NewEnviroment(tiles)
 	var evaluator = NewFitnessEvaluator(enviroment)
 	var actionSelector = NewActionSelector()
-	var executor = NewTaskExecutor(enviroment, evaluator, actionSelector, STEPS)
+	var executor = NewTaskExecutor(enviroment, evaluator, actionSelector, arguments.steps)
 	var selector = NewParentSelector()
 	var stats = NewStatsGatherer(enviroment, selector)
-	var mutator = NewMutator(NewGaussMutationStrategy(MUTATION_STRENGTH))
+	var mutator = NewMutator(NewGaussMutationStrategy(arguments.mutationStrength))
 
 	// Factories and generators
 	var positionGenerator = NewPositionGenerator(enviroment)
@@ -29,13 +25,10 @@ func main() {
 	var nnReproductionFactory = NewNNReproductionFactory(NewArithmeticCrossoverStrategy())
 	var populationRepFactory = NewPopulationReproductionFactory(positionGenerator, nnReproductionFactory, selector)
 	var populationRndFactory = NewPopulationRandomFactory(positionGenerator, nnRandomFactory)
+	var firstPopulation = populationRndFactory.Make(arguments.popSize)
 
 	// Main loop
-	var firstPopulation = populationRndFactory.Make(POP_SIZE)
-	MainLoop(firstPopulation, populationRepFactory, executor, stats, mutator)
-}
-
-func MainLoop(population []*Microbe, populationFactory *PopulationReproductionFactory, executor *TaskExecutor, stats *StatsGatherer, mutator *Mutator) {
+	var population = firstPopulation
 	var saved = 0
 	var averageFitness = 0.0
 	var highestFitness = 0.0
@@ -45,15 +38,15 @@ func MainLoop(population []*Microbe, populationFactory *PopulationReproductionFa
 		executor.ExecuteTask(population)
 
 		// Print stats
-		if (generation%PRINT_EVERY_NTH_GEN == 0) || saved == POP_SIZE {
+		if (generation%PRINT_EVERY_NTH_GEN == 0) || saved == arguments.popSize {
 			saved = stats.CountMicrobesInSafeZone(population)
 			averageFitness = stats.GetAverageFitness(population)
 			highestFitness = stats.GetHighestFitness(population)
-			fmt.Printf("%5d.  |  %3d/%d  |  %2.2f |  %2.2f\n", generation, saved, POP_SIZE, averageFitness, highestFitness)
+			fmt.Printf("%5d.  |  %3d/%d  |  %2.2f |  %2.2f\n", generation, saved, arguments.popSize, averageFitness, highestFitness)
 		}
 
 		// Create new generation
-		population = populationFactory.Make(population, POP_SIZE)
+		population = populationRepFactory.Make(population, arguments.popSize)
 		mutator.MutatePopulation(population)
 		generation++
 	}
