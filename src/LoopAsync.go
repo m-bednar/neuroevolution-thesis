38bb19e65ef79c2fde13debc1841c7ab4fdf2303
mar-bednar <mbedna8@seznam.cz>
2023-03-20 21:44:04 +0100

    fix

--- src/LoopAsync.go
+++ src/LoopAsync.go
@@ -10,10 +10,10 @@ type LoopFunc[Value any] func(index int, value Value)
 func LoopAsync[Value any](items []Value, loopFunc LoopFunc[Value]) {
 	var wg = sync.WaitGroup{}
 	var limit = runtime.NumCPU() + 1
-	var limiter = make(chan interface{}, limit)
+	var limiter = make(chan struct{}, limit)
 	wg.Add(len(items))
 	for i, v := range items {
-		limiter <- nil
+		limiter <- struct{}{}
 		go func(index int, value Value) {
 			loopFunc(index, value)
 			<-limiter
